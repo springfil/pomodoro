6424847cc38f35e9fede9ee8e2412c1684486deb
springfil <119633315+springfil@users.noreply.github.com>
2023-05-04 16:40:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -83,4 +83,8 @@
     }
     }
 
-    START_BUTTON.addEventListener("click", startTimer);
+    START_BUTTON.addEventListener("click", () => {
+    startTimer();
+    const audio = new Audio("yamade.mp3");
+    audio.play();
+    });
